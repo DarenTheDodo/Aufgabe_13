b24f1358a5fab222d6af0412b18c2fa0071c533c
DarenTheDodo <kay.zander@outlook.com>
2021-12-14 12:58:22 +0100

    fix output and add playagain button

--- main.c
+++ main.c
@@ -72,38 +72,39 @@ int fehlerAusgabe(int fehler){
 
 
 int main() {
-    char wort[255] = "";
-    int alter;
-    int rateVersuche;
-    int fehler = 12;
-    int richtigGeraten = 0;
-    char eingabe;
+    int bool = 1;
+    while (bool == 1) {
+        char wort[255] = "";
+        int alter;
+        int rateVersuche;
+        int fehler = 12;
+        int richtigGeraten = 0;
+        char eingabe;
 
-    //  Set Lösungswort
-    printf("L\x94sungswort eingeben: ");
-    scanf("%s", wort);
+        //  Set Lösungswort
+        printf("L\x94sungswort eingeben: ");
+        scanf(" %s", wort);
 
-    // Set Lösungswort to lower case
-    for (int i = 0; i < strlen(wort); i++){
-        wort[i] = tolower(wort[i]);
-    }
+        // Set Lösungswort to lower case
+        for (int i = 0; i < strlen(wort); i++) {
+            wort[i] = tolower(wort[i]);
+        }
 
-    printf("%s\n", wort);
+        printf("\n");
 
-    // Set placerHolder string "____", genauso lange wie die Antwort
-    //    F---h
-    char lw[strlen(wort)];
-    for (int i = 0; i < strlen(wort); i++){
-        lw[i] = '_';
-    }
-    printf(" %s\n", lw);
-
+        // Set placerHolder string "____", genauso lange wie die Antwort
+        //    F---h
+        char lw[strlen(wort)];
+        for (int i = 0; i < strlen(wort); i++) {
+            lw[i] = '_';
+        }
 
 
-    // Set alter
-    printf("Alter eingeben: ");
-    scanf("%i", &alter);
-    rateVersuche = versuche(alter);
+
+        // Set alter
+        printf("Alter eingeben: ");
+        scanf("%i", &alter);
+        rateVersuche = versuche(alter);
 
 
     // Errechne Rateversuche
@@ -112,27 +113,27 @@ int main() {
     fehlerAusgabe(fehler);
 
 
-    //Aufgabe8
-    //Schleife läuft solange wie fehler oder anzahl buchstaben nicht erreicht ist
-    while (fehler != 12 && richtigGeraten != strlen(wort)) {
-        int treffer = 0;
+        //Aufgabe8
+        //Schleife läuft solange wie fehler oder anzahl buchstaben nicht erreicht ist
+        while (fehler != 12 && richtigGeraten != strlen(wort)) {
+            int treffer = 0;
 
+            printf("%s\n\n", lw);
+            printf("Eingabe: ");
+            scanf(" %c", &eingabe);
+            eingabe = tolower(eingabe);
 
-        printf("Eingabe: ");
-        scanf(" %c", &eingabe);
-        eingabe = tolower(eingabe);
+            //vergleiche eingabe mit Antwort
+            for (int i = 0; i < strlen(wort); i++) {
 
-        //vergleiche eingabe mit Antwort
-        for (int i = 0; i < strlen(wort); i++){
-
-            if (wort[i] == eingabe){
-                lw[i] = eingabe;
-                treffer++;
+                if (wort[i] == eingabe) {
+                    lw[i] = eingabe;
+                    treffer++;
+                }
             }
-        }
 
-        //Zähle treffer hoch
-        //Todo Switch oder if Doz Fragen!
+            //Zähle treffer hoch
+            //Todo Switch oder if Doz Fragen!
 //        if(treffer > 0){
 //            richtigGeraten = richtigGeraten + treffer;
 //            fehlerAusgabe(fehler);
@@ -142,28 +143,38 @@ int main() {
 //            fehlerAusgabe(fehler);
 //        }
 
-        switch (treffer){
-            case 0:
-                fehler++;
-                fehlerAusgabe(fehler);
-                break;
-            default:
-                richtigGeraten = richtigGeraten + treffer;
-                fehlerAusgabe(fehler);
-                break;
+            switch (treffer) {
+                case 0:
+                    fehler++;
+                    rateVersuche--;
+                    fehlerAusgabe(fehler);
+                    break;
+                default:
+                    richtigGeraten = richtigGeraten + treffer;
+                    fehlerAusgabe(fehler);
+                    break;
+            }
+
+            printf("Fehler: %i\n", fehler);
+            if (rateVersuche > 1) {
+                printf("Sie haben noch %i Versuche \x81""brig \n", rateVersuche);
+            } else if (rateVersuche == 1) {
+                printf("Sie haben noch %i Versuch \x81""brig \n", rateVersuche);
+            } else {
+                printf("\n");
+            }
+
         }
-
-        printf("Fehler: %i \n", fehler);
-        printf("%s\n\n", lw);
-
+        if (richtigGeraten == strlen(wort)) {
+            printf("SIE HABEN GEWONNEN!\n");
+        } else {
+            printf("SIE HABEN VERLOREN!\n");
+            printf("Das Wort lautete: %s\n", wort);
+        }
+        printf("Dr\x81""cken sie 1 um nochmal zu spielen!\n");
+        scanf("%i", &bool);
     }
-    if(richtigGeraten == strlen(wort)){
-        printf("SIE HABEN GEWONNEN!");
-    }else{
-        printf("SIE HABEN VERLOREN!");
-    }
-
-
+ printf("Danke f\x81""rs Spielen!");
 
     return 0;
 }
